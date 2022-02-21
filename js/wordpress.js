0dd0e709169bc9b1cfa8c39f9370bf26a163c7bb
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-02-21 17:34:25 +0100

    secondendpoint

--- js/wordpress.js
+++ js/wordpress.js
@@ -1,4 +1,4 @@
-const endPoint = "https://danielfraisland.dk/wp-json/wp/v2/posts/?categories=9&per_page=3"
+const endPoint = "https://wordpress.danielfraisland.dk/wp-json/wp/v2/posts/?categories=2&per_page=3"
 
 fetch( endPoint ).then(
     response => {
@@ -12,10 +12,35 @@ fetch( endPoint ).then(
             <h3 class="cvtitle">            
                 ${data[i].title.rendered}
             </h3>
-            <div id="post-content">
+            <div class="post-content">
               ${data[i].content.rendered}
             <div>
             </article>`
         }
 })
 console.log(endPoint);
+
+
+
+const secondendPoint = "https://wordpress.danielfraisland.dk/wp-json/wp/v2/posts?_embed&categories=3"
+
+fetch( secondendPoint ).then(
+    response => {
+      return response.json(); // get JSON data
+    }).then(data => {
+
+        for (let i = 0; i < data.length; i++) {
+
+          //Posts
+          viewProjects.innerHTML += `<article>
+            <h3 class="cvtitle">            
+                ${data[i].title.rendered}
+            </h3>
+            <div class="post-content">
+              ${data[i].content.rendered}
+            <div>
+            </article>`
+        }
+})
+console.log(secondendPoint);
+//https://wordpress.danielfraisland.dk/wp-json/wp/v2/posts?_embed&categories=3

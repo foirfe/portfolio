82bbb8f37a74d4d32ed9d3d5f4a08b835e5260c0
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-02-22 17:02:06 +0100

    22 Februar

--- js/wordpress.js
+++ js/wordpress.js
@@ -22,25 +22,44 @@ console.log(endPoint);
 
 
 
-const secondendPoint = "https://wordpress.danielfraisland.dk/wp-json/wp/v2/posts?_embed&categories=3"
+const projectpostsurl = "https://wordpress.danielfraisland.dk/wp-json/wp/v2/posts?_embed&categories=3";
+//
+fetch(projectpostsurl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (projectposts) {
+    console.log(projectposts);
+    appendPosts(projectposts);
+  });
 
-fetch( secondendPoint ).then(
-    response => {
-      return response.json(); // get JSON data
-    }).then(data => {
+// Append Projects Posts to the DOM
+function appendPosts(projectposts) {
+  let htmlTemplate = "";
+  for (let post of projectposts) {
+    console.log(post);
+    htmlTemplate += /*html*/`
+      <div class="project">
+      <img src="${getFeaturedImageUrl(post)}" class="projectimage">
+        <h3>${post.title.rendered}</h3>
+        <div>
+        <p>${post.content.rendered}</p>
+        <a href="${post.x_metadata.Website}"><img src="images/icons/website.png" alt="View the Website" class="icon"></a>
+        <a href="${post.x_metadata.Github}"><img src="images/icons/github.png" alt="View Code on Github" class="icon"></a>
+        </div>
+      </div>
+    `;
+  }
+  document.querySelector('#viewProjects').innerHTML = htmlTemplate;
+}
 
-        for (let i = 0; i < data.length; i++) {
-
-          //Posts
-          viewProjects.innerHTML += `<article>
-            <h3 class="cvtitle">            
-                ${data[i].title.rendered}
-            </h3>
-            <div class="post-content">
-              ${data[i].content.rendered}
-            <div>
-            </article>`
-        }
-})
-console.log(secondendPoint);
+// Get the featured image url
+function getFeaturedImageUrl(post) {
+  let imageUrl = "";
+  if (post._embedded['wp:featuredmedia']) {
+    imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
+  }
+  return imageUrl;
+}
 //https://wordpress.danielfraisland.dk/wp-json/wp/v2/posts?_embed&categories=3
+
